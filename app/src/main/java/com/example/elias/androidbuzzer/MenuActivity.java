4602aeba052be445a3ce0312d0fc5f4fde2d435f
edcarter <edcarter@ualberta.ca>
2015-09-13 16:01:08 -0600

    Added activities for different app modes

--- app/src/main/java/com/example/elias/androidbuzzer/MenuActivity.java
+++ app/src/main/java/com/example/elias/androidbuzzer/MenuActivity.java
@@ -1,9 +1,11 @@
 package com.example.elias.androidbuzzer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -33,5 +35,10 @@ public class MenuActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void navigate_to_reaction_tester(View view) {
+        Intent intent = new Intent(this, ReactionActivity.class);
+        startActivity(intent);
     }
 }
