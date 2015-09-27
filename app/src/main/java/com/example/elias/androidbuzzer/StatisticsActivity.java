a525d313fdc5f8da35a731fcf24a2de1d8a26728
edcarter <edcarter@ualberta.ca>
2015-09-27 15:35:21 -0600

    added super hacked statistics formatter

--- app/src/main/java/com/example/elias/androidbuzzer/StatisticsActivity.java
+++ app/src/main/java/com/example/elias/androidbuzzer/StatisticsActivity.java
@@ -1,14 +1,13 @@
 package com.example.elias.androidbuzzer;
 
-import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Stack;
 
 public class StatisticsActivity extends AppCompatActivity {
@@ -30,20 +29,123 @@ public class StatisticsActivity extends AppCompatActivity {
         statistics.addAll(formatTwoPlayerStats(model));
         statistics.addAll(formatThreePlayerStats(model));
         statistics.addAll(formatFourPlayerStats(model));
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, statistics);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, statistics);
+
+        //http://blog.denevell.org/android-SimpleExpandableListAdapter-example.html
         listView.setAdapter(arrayAdapter);
     }
 
+
+
     private ArrayList<String> formatSinglePlayerStats(StatisticsModel model) {
-        return new ArrayList<>();
+        ArrayList<String> formattedStats = new ArrayList<>();
+        formattedStats.addAll(getMinimumReactionTimes(model));
+        formattedStats.addAll(getMaximumReactionTimes(model));
+        formattedStats.addAll(getAverageReactionTimes(model));
+        formattedStats.addAll(getMedianReactionTimes(model));
+        return formattedStats;
+    }
+
+    private ArrayList<String> getMinimumReactionTimes(StatisticsModel model){
+        ArrayList<String> formattedStats = new ArrayList<>();
+        Stack<Integer> reactionTimes = model.singlePlayerReactionTimesMs;
+        Integer minimumReactionTime = reactionTimes.peek();
+        for (int i = 0; i < reactionTimes.size(); i++){
+            if (reactionTimes.elementAt(i) < minimumReactionTime) minimumReactionTime = reactionTimes.elementAt(i);
+            if (i == 9){
+                formattedStats.add("1 Player: Last 10 Tries Minimum Reaction Time: " + minimumReactionTime + " ms");
+            } else if (i == 99){
+                formattedStats.add("1 Player: Last 100 Tries Minimum Reaction Time: " + minimumReactionTime + " ms");
+            }
+        }
+        if (reactionTimes.size() < 10){
+            formattedStats.add("1 Player: Last 10 Tries Minimum Reaction Time: " + minimumReactionTime + " ms");
+        }
+        if (reactionTimes.size() < 100){
+            formattedStats.add("1 Player: Last 100 Tries Minimum Reaction Time: " + minimumReactionTime + " ms");
+        }
+        formattedStats.add("1 Player: Global Minimum Reaction Time: " + minimumReactionTime + " ms");
+
+        return formattedStats;
+    }
+
+    private ArrayList<String> getMaximumReactionTimes(StatisticsModel model){
+        ArrayList<String> formattedStats = new ArrayList<>();
+        Stack<Integer> reactionTimes = model.singlePlayerReactionTimesMs;
+        Integer maximumReactionTime = reactionTimes.peek();
+        for (int i = 0; i < reactionTimes.size(); i++){
+            if (reactionTimes.elementAt(i) > maximumReactionTime) maximumReactionTime = reactionTimes.elementAt(i);
+            if (i == 9){
+                formattedStats.add("1 Player: Last 10 Tries Maximum Reaction Time: " + maximumReactionTime + " ms");
+            } else if (i == 99){
+                formattedStats.add("1 Player: Last 100 Tries Maximum Reaction Time: " + maximumReactionTime + " ms");
+            }
+        }
+        if (reactionTimes.size() < 10){
+            formattedStats.add("1 Player: Last 10 Tries Maximum Reaction Time: " + maximumReactionTime + " ms");
+        }
+        if (reactionTimes.size() < 100){
+            formattedStats.add("1 Player: Last 100 Tries Maximum Reaction Time: " + maximumReactionTime + " ms");
+        }
+        formattedStats.add("1 Player: Global Maximum Reaction Time: " + maximumReactionTime + " ms");
+
+        return formattedStats;
+    }
+
+    private ArrayList<String> getAverageReactionTimes(StatisticsModel model){
+        ArrayList<String> formattedStats = new ArrayList<>();
+        Stack<Integer> reactionTimes = model.singlePlayerReactionTimesMs;
+        Integer accumulator = 0;
+        for (int i = 0; i < reactionTimes.size(); i++){
+            accumulator += reactionTimes.elementAt(i);
+            if (i == 9){
+                formattedStats.add("1 Player: Last 10 Tries Average Reaction Time: " + accumulator/i + " ms");
+            } else if (i == 99){
+                formattedStats.add("1 Player: Last 100 Tries Average Reaction Time: " + accumulator/i + " ms");
+            }
+        }
+        if (reactionTimes.size() < 10){
+            formattedStats.add("1 Player: Last 10 Tries Average Reaction Time: " + accumulator/reactionTimes.size() + " ms");
+        }
+        if (reactionTimes.size() < 100){
+            formattedStats.add("1 Player: Last 100 Tries Average Reaction Time: " + accumulator/reactionTimes.size() + " ms");
+        }
+        formattedStats.add("1 Player: Global Average Reaction Time: " + accumulator/reactionTimes.size() + " ms");
+
+        return formattedStats;
+
+    }
+
+    private ArrayList<String> getMedianReactionTimes(StatisticsModel model){
+        ArrayList<String> formattedStats = new ArrayList<>();
+        Stack<Integer> reactionTimes = model.singlePlayerReactionTimesMs;
+        ArrayList<Integer> reactionTimesList = new ArrayList<>(reactionTimes);
+        Collections.sort(reactionTimesList);
+        for (int i = 0; i < reactionTimes.size(); i++){
+            if (i == 9){
+                formattedStats.add("1 Player: Last 10 Tries Median Reaction Time: " + (reactionTimesList.get(5) + reactionTimesList.get(6))/2 + " ms");
+            } else if (i == 99){
+                formattedStats.add("1 Player: Last 100 Tries Median Reaction Time: " + (reactionTimesList.get(50) + reactionTimesList.get(51))/ 2 + " ms");
+            }
+        }
+        if (reactionTimes.size() < 10){
+            formattedStats.add("1 Player: Last 10 Tries Median Reaction Time: " + reactionTimesList.get(reactionTimes.size() / 2) + " ms");
+        }
+        if (reactionTimes.size() < 100){
+            formattedStats.add("1 Player: Last 100 Tries Median Reaction Time: " + reactionTimesList.get(reactionTimes.size() / 2) + " ms");
+        }
+        formattedStats.add("1 Player: Global Median Reaction Time: " + reactionTimesList.get(reactionTimes.size() / 2) + " ms");
+
+        return formattedStats;
+
     }
 
     private ArrayList<String> formatTwoPlayerStats(StatisticsModel model) {
         Stack<Integer> firstPlayerReaction = model.twoPlayerFirstBuzzer;
         Integer playerOneReactedFirst = 0;
         Integer playerTwoReactedFirst = 0;
-        for (Integer firstPlayer : firstPlayerReaction){
-            if (firstPlayer == 0){
+        for (Integer firstPlayer : firstPlayerReaction) {
+            if (firstPlayer == 0) {
                 playerOneReactedFirst++;
             } else {
                 playerTwoReactedFirst++;
@@ -60,8 +162,8 @@ public class StatisticsActivity extends AppCompatActivity {
         Integer playerOneReactedFirst = 0;
         Integer playerTwoReactedFirst = 0;
         Integer playerThreeReactedFirst = 0;
-        for (Integer firstPlayer : firstPlayerReaction){
-            if (firstPlayer == 0){
+        for (Integer firstPlayer : firstPlayerReaction) {
+            if (firstPlayer == 0) {
                 playerOneReactedFirst++;
             } else if (firstPlayer == 1) {
                 playerTwoReactedFirst++;
@@ -82,8 +184,8 @@ public class StatisticsActivity extends AppCompatActivity {
         Integer playerTwoReactedFirst = 0;
         Integer playerThreeReactedFirst = 0;
         Integer playerFourReactedFirst = 0;
-        for (Integer firstPlayer : firstPlayerReaction){
-            if (firstPlayer == 0){
+        for (Integer firstPlayer : firstPlayerReaction) {
+            if (firstPlayer == 0) {
                 playerOneReactedFirst++;
             } else if (firstPlayer == 1) {
                 playerTwoReactedFirst++;
