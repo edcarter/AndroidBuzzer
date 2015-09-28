0d595762bd5daa67508e60325581c36927989746
edcarter <edcarter@ualberta.ca>
2015-09-28 11:23:10 -0600

    updated licences

--- app/src/main/java/com/example/elias/androidbuzzer/StatisticsEngine.java
+++ app/src/main/java/com/example/elias/androidbuzzer/StatisticsEngine.java
@@ -1,10 +1,24 @@
+/*
+Copyright 2015 Elias Carter
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package com.example.elias.androidbuzzer;
 
 import android.content.Context;
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -14,9 +28,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
 
-/**
- * Created by elias on 26/09/15.
- */
+
 public class StatisticsEngine {
 
     private static String savePath = "ReactionStatistics";
